5de472b7fbab308bc0dea6072c3119d20abe84e9
nelson <nelsonmunyua8@gmail.com>
2026-02-07 17:17:55 +0300

    Add Render deployment files

--- app.py
+++ app.py
@@ -17,13 +17,13 @@ load_dotenv(override=True)
 app = Flask(__name__)
 
 # Enable CORS for all routes with proper configuration
-CORS(app)
-# CORS(app, 
-#     resources={r"/api/*": {"origins": ["http://localhost:5173", "http://localhost:5174", "http://127.0.0.1:5173", "http://127.0.0.1:5174"]}},
-#     supports_credentials=True,
-#     methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-#     allow_headers=["Content-Type", "Authorization", "Access-Control-Allow-Credentials"]
-# )
+#CORS(app)
+CORS(app, 
+    resources={r"/*": {"origins": ["http://localhost:5173"]}},
+    supports_credentials=True,
+    methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allow_headers=["Content-Type", "Authorization", "Access-Control-Allow-Credentials"]
+)
 
 # Load configuration
 app.config.from_object(Config)
